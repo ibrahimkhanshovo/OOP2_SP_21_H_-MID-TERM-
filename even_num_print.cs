4693d8870e221031df5ecf78c988d7b03fcee069
Shovo <khanshuvo7444@gmail.com>
2021-02-07 12:25:33 +0600

    even_num_print

---
+++ even_num_print.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace printevennumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class Program
+        {
+            static void Main(string[] args)
+            {
+                int i = 0;
+
+                Console.WriteLine("Even Numbers :");
+                for (i = 1; i <= 30; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+    }
+}
