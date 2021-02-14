de314e0079727c6f2cd153fcaae333cefb99ec48
Shovo <khanshuvo7444@gmail.com>
2021-02-14 14:00:58 +0600

    Array 03

---
+++ Array03.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] arr = new int[100]; ;
+                int i, j, k, size, isUnique;
+
+              
+                Console.WriteLine(" : ");
+                size = Convert.ToInt32(Console.ReadLine());
+               
+                Console.WriteLine(" : ");
+                for (i = 0; i < size; i++)
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+             
+
+                for (i = 0; i < size; i++)
+                {
+                 
+                    isUnique = 1;
+
+                    for (j = i + 1; j < size; j++)
+                    {
+
+                  
+
+                        if (arr[i] == arr[j])
+                        {
+                           
+                            for (k = j; k < size - 1; k++)
+                            {
+                                arr[k] = arr[k + 1];
+                            }
+
+                            size--;
+                            j--;
+                            isUnique = 0;
+                        }
+                    }
+
+                  
+                    if (isUnique != 1)
+                    {
+                        for (j = i; j < size - 1; j++)
+                        {
+                            arr[j] = arr[j + 1];
+                        }
+
+                        size--;
+                        i--;
+                    }
+                }
+
+       
+                Console.WriteLine(" : ");
+                for (i = 0; i < size; i++)
+                {
+                    Console.WriteLine(arr[i] + "\t");
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+    }
+}
