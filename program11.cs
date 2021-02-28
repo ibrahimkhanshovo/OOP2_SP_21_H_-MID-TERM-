3faae81fec33cc09d4434633865faf734906cb50
Shovo <khanshuvo7444@gmail.com>
2021-02-28 14:27:27 +0600

    Student & Department

---
+++ program11.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department d1 = new Department("Computer Science and Engineering","CSE");
+            Student S1 = new Student("Mohammad Abir", "10001110", 3.00f);
+            S1.showDetails();
+        }
+    }
+}
